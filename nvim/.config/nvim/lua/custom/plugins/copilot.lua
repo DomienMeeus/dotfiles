6a0e9499b9d7ca3d2354046ae9c26941096c8f9f
zl-domien-meeus <domien.meeus@zinderlabs.com>
2025-12-19 19:19:33 +0100

    ai implementatio: sidekick & copilot

---
+++ nvim/.config/nvim/lua/custom/plugins/copilot.lua
@@ -0,0 +1,14 @@
+return {
+  'zbirenbaum/copilot.lua',
+  dependencies = {
+    'copilotlsp-nvim/copilot-lsp', -- (optional) for NES functionality
+  },
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup {
+      suggestion = { enabled = true }, -- Disable default ghost text
+      panel = { enabled = true }, -- Disable default panel
+    }
+  end,
+}
